12522f7f3ad1e4abc877169958a0127c3f8976b6
nanchengcyu <1146137024@qq.com>
2024-06-10 00:25:45 +0800

    项目初始化 环境搭建

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
+//引入图标组件
+import { Plus } from '@element-plus/icons-vue'
+import SvgIcon from '@/components/SvgIcon/index.vue'
 </script>
 
 <template>
- <h1>H1</h1>
+  <h1>H1</h1>
+  <el-button type="primary" size="default" :icon="Plus">主要</el-button>
+  <hr />
+  <svg>
+    <use xlink:href="#icon-phone"></use>
+  </svg>
+  <svg-icon name="home" color="pink" width="00px"></svg-icon>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//配置国际化中文
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+import 'virtual:svg-icons-register'
+app.mount('#app')
